c0a4a854166e6da63ac365612b35ca98fc19e757
walshw <williamdouglaswalsh@gmail.com>
2022-03-15 01:12:53 -0600

    Switched MultiplicationTokenizer.js to camelcase

--- miner.js
+++ miner.js
@@ -1,5 +1,5 @@
 const axios = require('axios').default;
-const tokenizer = require('./MultiplicationTokenizer');
+const tokenizer = require('./multiplicationTokenizer');
 
 const url = 'https://engine.freerice.com/games?lang=en';
 const payload = { "category": "66f2a9aa-bac2-5919-997d-2d17825c1837", "level": 1, "user": null };
