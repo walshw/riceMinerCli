b8c58f414c85b5415bc4479e744299cdde5d61ad
walshw <williamdouglaswalsh@gmail.com>
2022-03-19 08:38:22 -0600

    Removed unused code and comments

--- miner.js
+++ miner.js
@@ -1,12 +1,6 @@
 const axios = require('axios').default;
 const tokenizer = require('./multiplicationTokenizer');
 
-const url = 'https://engine.freerice.com/games?lang=en';
-const payload = { "category": "66f2a9aa-bac2-5919-997d-2d17825c1837", "level": 1, "user": null };
-
-
-
-
 class Session {
     url = 'https://engine.freerice.com/games?lang=en';
     payload = { "category": "66f2a9aa-bac2-5919-997d-2d17825c1837", "level": 1, "user": null };
@@ -44,63 +38,6 @@ class Session {
     }
 }
 
-
-async function main() {
-    // res.data['data']['attributes']['question']['text']
-    axios.post(url, payload).then(async res => {
-        const sessionId = res.data['data']['id'];
-
-        let questionId = res.data['data']['attributes']['question_id'];
-
-        const question = res.data['data']['attributes']['question']['text'];
-        const options = res.data['data']['attributes']['question']['options'];
-
-
-        const answer = tokenizer.multiply((question));
-        let answerId = findAnswer(answer, options);
-
-        // console.log(question);
-        // console.log(options);
-
-        // console.log(answerId + " " + answer);
-
-
-
-        // Go through here and go through the old way we got it to work and see why it worked
-
-
-
-
-        // while (true) {
-        const questionPayload = { "answer": answerId, "question": questionId, "user": null };
-
-        axios.patch(`https://engine.freerice.com/games/${sessionId}/answer?lang=en`, questionPayload).then(x => {
-            x.data
-            console.log(x.data['data']);
-        }).catch(err => {
-            err
-        });
-
-        // await AnswerQuestion(sessionId, questionPayload);
-        // }
-    }).catch(err => console.log(err));
-}
-
-
-async function AnswerQuestion(sessionId, payload) {
-    const promise = await axios.patch(`https://engine.freerice.com/games/${sessionId}/answer?lang=en`, payload);
-    const questionId = promise.data['data']['attributes']['question']['text'];
-    const question = res.data['data']['attributes']['question']['text'];
-    const options = res.data['data']['attributes']['question']['options'];
-    const answer = tokenizer.multiply((question));
-    const answerId = findAnswer(answer, options);
-
-    return new Answer(answerId, questionId);
-}
-
-
-// main();
-
 (async () => {
     const sesh = new Session();
     await sesh.init();
@@ -116,13 +53,3 @@ async function AnswerQuestion(sessionId, payload) {
 
 // Probably want to have a recursive function that calls itself on answering the question (check if we get a next question link)
 // Want to have the possibility of scaling to have multiple requests going off at the same time
-
-
-
-// have to make a PATCH to https://engine.freerice.com/games/<question_id>????? or is it just the ID of the promise?/answer?lang=en
-
-// request to the id with the question_id in the payload, I THINK
-
-function findAnswer(answer, options) {
-    return options.find(option => option['text'] === answer + '')['id']
-}
